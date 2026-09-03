8f5c1d42de162f3b10591b3045957a43cc236b7f
Janier Soares <janierbilhalvasoares@gmail.com>
2026-09-03 14:25:26 -0300

    Aula - Escopo de funções em Python

---
+++ SESSÃO_4/aula68.py
@@ -0,0 +1,27 @@
+# AULA 68
+'''
+Escopo de funções em Python
+Escopo significa o local onde aquele código pode atingir
+Existe o escopo global e local
+O escopo global é o escopo onde apenas nomes do mesmo local
+podem ser alcançados.
+'''
+
+x = 1
+
+def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        y = 2
+        x = 11
+        print(x, y)
+    outra_funcao()
+    print(x)
+
+print(x)
+
+escopo()
+
+print(x)
