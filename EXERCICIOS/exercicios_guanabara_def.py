9c54b5d5ffb9bbc16ea04da9ed6f887d341a637e
Janier Soares <janierbilhalvasoares@gmail.com>
2026-09-14 14:51:50 -0300

    Exercicio Guanabara - Funcoes(def).

---
+++ EXERCICIOS/exercicios_guanabara_def.py
@@ -0,0 +1,9 @@
+# EXERCICIO 01 GUANABARA - FUNÇÕES(DEF):
+
+def area(largura, comprimento):
+    terreno = largura * comprimento
+    print(f'LARGURA (m): {largura}')
+    print(f'COMPRIMENTO (m): {comprimento}')
+    print(f'A área de um terreno {largura}x{comprimento} é de {terreno}m².')
+
+area(4.5, 8)
